5815cabd3e168d1352ad48f5a721e451b0d1df71
laok2020 <lyx_haven@hotmail.com>
2020-08-20 11:11:33 -0400

    wallet icons

--- src/features/common/sections/SectionWallet.js
+++ src/features/common/sections/SectionWallet.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // @material-ui/core components
 import DialogContent from "@material-ui/core/DialogContent";
 import CircularProgress from "@material-ui/core/CircularProgress";
@@ -6,15 +6,20 @@ import Typography from "@material-ui/core/Typography";
 // core components
 import Button from "components/CustomButtons/Button.js";
 
-import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
+import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
 import {
   NoEthereumProviderError,
-  UserRejectedRequestError as UserRejectedRequestErrorInjected
-} from '@web3-react/injected-connector'
-import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector'
-import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from '@web3-react/frame-connector'
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
+} from "@web3-react/injected-connector";
+import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from "@web3-react/walletconnect-connector";
+import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from "@web3-react/frame-connector";
 
-import { useAccount, useEagerConnect, useInactiveListener, useCloseModal } from '../redux/hooks';
+import {
+  useAccount,
+  useEagerConnect,
+  useInactiveListener,
+  useCloseModal,
+} from "../redux/hooks";
 
 import {
   injected,
@@ -28,8 +33,8 @@ import {
   fortmatic,
   portis,
   squarelink,
-  torus
-} from '../../configure';
+  torus,
+} from "../../configure";
 
 const connectorsByName = {
   MetaMask: injected,
@@ -43,155 +48,240 @@ const connectorsByName = {
   Portis: portis,
   Squarelink: squarelink,
   Torus: torus,
-  Authereum: authereum
-}
+  Authereum: authereum,
+};
 
 function getErrorMessage(error) {
   if (error instanceof NoEthereumProviderError) {
-    return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
+    return "No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.";
   } else if (error instanceof UnsupportedChainIdError) {
-    return "You're connected to an unsupported network."
+    return "You're connected to an unsupported network.";
   } else if (
     error instanceof UserRejectedRequestErrorInjected ||
     error instanceof UserRejectedRequestErrorWalletConnect ||
     error instanceof UserRejectedRequestErrorFrame
   ) {
-    return 'Please authorize this website to access your Ethereum account.'
+    return "Please authorize this website to access your Ethereum account.";
   } else {
-    console.error(error)
-    return 'An unknown error occurred. Check the console for more details.'
+    console.error(error);
+    return "An unknown error occurred. Check the console for more details.";
   }
 }
 
 export default function SectionWallet() {
-  const context = useWeb3React()
-  const { connector, library, chainId, account, activate, deactivate, active, error } = context
+  const context = useWeb3React();
+  const {
+    connector,
+    library,
+    chainId,
+    account,
+    activate,
+    deactivate,
+    active,
+    error,
+  } = context;
 
   const { setAccount } = useAccount();
 
   const { closeModal } = useCloseModal();
 
   // handle logic to recognize the connector currently being activated
-  const [activatingConnector, setActivatingConnector] = useState()
+  const [activatingConnector, setActivatingConnector] = useState();
   useEffect(() => {
     if (activatingConnector && activatingConnector === connector) {
-      setActivatingConnector(undefined)
+      setActivatingConnector(undefined);
     }
-  }, [activatingConnector, connector])
+  }, [activatingConnector, connector]);
 
   useEffect(() => {
     if (account && active && library) {
-
       const data = {
         account,
-        provider: library.provider
-      }
+        provider: library.provider,
+      };
       setAccount(data);
       closeModal();
     }
   }, [account, active, context, library]);
 
   // handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
-  const triedEager = useEagerConnect()
+  const triedEager = useEagerConnect();
 
   // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
-  useInactiveListener(!triedEager || !!activatingConnector)
+  useInactiveListener(!triedEager || !!activatingConnector);
 
-  const width = window.innerWidth
+  const width = window.innerWidth;
 
   return (
-    
-      <DialogContent>
-        <div
-          style={{ display: 'flex', flexWrap: 'wrap', justifyContent: (width > 650 ? 'space-between' : 'center'), alignItems: 'center' }}
-        >
-          {Object.keys(connectorsByName).map(name => {
-            const currentConnector = connectorsByName[name]
-            const activating = currentConnector === activatingConnector
-            const connected = currentConnector === connector
-            const disabled = !triedEager || !!activatingConnector || connected || !!error
+    <DialogContent>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: width > 650 ? "space-between" : "center",
+          alignItems: "center",
+        }}
+      >
+        {Object.keys(connectorsByName).map((name) => {
+          const currentConnector = connectorsByName[name];
+          const activating = currentConnector === activatingConnector;
+          const connected = currentConnector === connector;
+          const disabled =
+            !triedEager || !!activatingConnector || connected || !!error;
 
-            return (
-              <div key={name} style={{ padding: '12px 0px', display: 'flex', justifyContent: 'space-between'  }}>
-                <Button
-                  style={{
-                    padding: '16px',
-                    backgroundColor: 'white',
-                    borderRadius: '1rem',
-                    border: '1px solid #E1E1E1',
-                    fontWeight: 500,
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    minWidth: '250px'
-                  }}
-                  variant='outlined'
-                  color='primary'
-                  disabled={disabled}
-                  onClick={() => {
-                    console.log('begin')
-                    setActivatingConnector(currentConnector)
-                    console.log(name)
-                    console.log(connectorsByName[name])
-                    activate(connectorsByName[name])
-                    console.log('close')
-                  }}
-                >
-                  <Typography style={ {
-                  margin: '0px 12px',
-                  color: 'rgb(1, 1, 1)',
+          // determine img url
+          let url = "";
+          let displayName = name;
+          switch (name) {
+            case "MetaMask":
+              url = require("../../../images/icn-metamask.svg");
+              break;
+            case "WalletConnect":
+              url = require("../../../images/walletConnectIcon.svg");
+              break;
+            case "TrustWallet":
+              url = require("../../../images/trustWallet.png");
+              break;
+            case "Portis":
+              url = require("../../../images/portisIcon.png");
+              break;
+            case "Fortmatic":
+              url = require("../../../images/fortmaticIcon.png");
+              break;
+            case "Ledger":
+              url = require("../../../images/icn-ledger.svg");
+              break;
+            case "Squarelink":
+              url = require("../../../images/squarelink.png");
+              break;
+            case "Trezor":
+              url = require("../../../images/trezor.png");
+              break;
+            case "Torus":
+              url = require("../../../images/torus.jpg");
+              break;
+            case "Authereum":
+              url = require("../../../images/icn-aethereum.svg");
+              break;
+            case "WalletLink":
+              displayName = "Coinbase Wallet";
+              url = require("../../../images/coinbaseWalletIcon.svg");
+              break;
+            case "Frame":
+              url = "";
+              break;
+          }
+
+          return (
+            <div
+              key={name}
+              style={{
+                padding: "12px 0px",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Button
+                style={{
+                  padding: "16px",
+                  backgroundColor: "white",
+                  borderRadius: "1rem",
+                  border: "1px solid #E1E1E1",
                   fontWeight: 500,
-                  fontSize: '1rem',
-                } }
-                    variant={ 'h3'}>
-                    { name }
-                  </Typography>
-                    { (!activating && !connected) && <img style={
-                      {
-                        position: 'absolute',
-                        right: '20px',
+                  display: "flex",
+                  justifyContent: "space-between",
+                  minWidth: "250px",
+                }}
+                variant="outlined"
+                color="primary"
+                disabled={disabled}
+                onClick={() => {
+                  console.log("begin");
+                  setActivatingConnector(currentConnector);
+                  console.log(name);
+                  console.log(connectorsByName[name]);
+                  activate(connectorsByName[name]);
+                  console.log("close");
+                }}
+              >
+                <Typography
+                  style={{
+                    margin: "0px 12px",
+                    color: "rgb(1, 1, 1)",
+                    fontWeight: 500,
+                    fontSize: "1rem",
+                  }}
+                  variant={"h3"}
+                >
+                  {displayName}
+                </Typography>
+                {!activating && !connected && (
+                  <img
+                    style={{
+                      position: "absolute",
+                      right: "20px",
 
-                        width: '30px',
-                        height: '30px'
-                      }
-                    } src={"url"} alt=""/> }
-                    {activating && <CircularProgress size={ 15 } style={{marginRight: '10px'}} />}
-                    {(!activating && connected) && <div style={{ background: '#4caf50', borderRadius: '10px', width: '10px', height: '10px', marginRight: '10px' }}></div>}
-                  {name}
-                </Button>
-              </div>
-            )
-          })}
-        </div>
-        <div style={{ width: '252px', margin: '12px 0px'  }}>
-        <Button style={ {
-            padding: '12px',
-            backgroundColor: 'white',
-            borderRadius: '20px',
-            border: '1px solid #E1E1E1',
+                      width: "30px",
+                      height: "30px",
+                    }}
+                    src={url}
+                    alt=""
+                  />
+                )}
+                {activating && (
+                  <CircularProgress size={15} style={{ marginRight: "10px" }} />
+                )}
+                {!activating && connected && (
+                  <div
+                    style={{
+                      background: "#4caf50",
+                      borderRadius: "10px",
+                      width: "10px",
+                      height: "10px",
+                      marginRight: "10px",
+                    }}
+                  ></div>
+                )}
+                {name}
+              </Button>
+            </div>
+          );
+        })}
+      </div>
+      <div style={{ width: "252px", margin: "12px 0px" }}>
+        <Button
+          style={{
+            padding: "12px",
+            backgroundColor: "white",
+            borderRadius: "20px",
+            border: "1px solid #E1E1E1",
             fontWeight: 500,
-            minWidth: '250px'
-          } }
-          variant='outlined'
-          color='primary'
-          onClick={() => { 
-            if(deactivate) {
-              deactivate()
+            minWidth: "250px",
+          }}
+          variant="outlined"
+          color="primary"
+          onClick={() => {
+            if (deactivate) {
+              deactivate();
             }
-            if(connector && connector.close) {
-              connector.close()
+            if (connector && connector.close) {
+              connector.close();
             }
-          }}>
-          <Typography style={ {
-              marginLeft: '12px',
-              fontWeight: '700',
-              color: '#DC6BE5'
-            } }
-            variant={ 'h5'}
-            color='primary'>
+          }}
+        >
+          <Typography
+            style={{
+              marginLeft: "12px",
+              fontWeight: "700",
+              color: "#DC6BE5",
+            }}
+            variant={"h5"}
+            color="primary"
+          >
             Deactivate
           </Typography>
         </Button>
       </div>
-      </DialogContent>     
+    </DialogContent>
   );
 }
