f5e83db37ee3ac346bf42eb8037e0a2de38fd328
jin10086 <igaojin@qq.com>
2020-08-20 10:02:53 +0800

    Update fetchClaimAbleTokens.js

--- src/features/web3/fetchClaimAbleTokens.js
+++ src/features/web3/fetchClaimAbleTokens.js
@@ -1,14 +1,19 @@
-import { crvDepositContractABI, earnContractABI } from "../configure";
+import { crvDepositContractABI, earnContractABI,strategyContractABI } from "../configure";
 import BigNumber from "bignumber.js";
 
 export const fetchClaimAbleTokens = async ({web3, account, contractAddress, isCrv}) => {
   // console.log(`=====================================fetchClaimAbleTokens begin=====================================`)
-  const contract = isCrv ? new web3.eth.Contract(crvDepositContractABI, "0xFA712EE4788C042e2B7BB55E6cb8ec569C4530c1") : new web3.eth.Contract(earnContractABI, contractAddress)
+  if (isCrv){
+    const contract = new web3.eth.Contract(crvDepositContractABI, "0xFA712EE4788C042e2B7BB55E6cb8ec569C4530c1");
+    const data = await contract.methods.claimable_tokens(contractAddress).call({ from: account });
+  }else{
+    const contract = new web3.eth.Contract(strategyContractABI, contractAddress);
+    const data = await contract.methods.balanceOfPendingReward().call({ from: account });
+  }
   // console.log(`
   //   account:${account}\n
   //   contractAddress:${contractAddress}\n
   // `)
-  const data = await contract.methods.claimable_tokens(contractAddress).call({ from: account });
   // console.log("data: "+data)
   const claimAbleTokens = new BigNumber(data).toNumber();
   // console.log(`
