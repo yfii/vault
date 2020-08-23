0a9e9e8fb8c930c09c51d4ba7c1132625e8fe732
iXiaowu <zhangxiaotong@yahoo.com>
2020-08-23 15:35:42 +0800

    update fetchBalances function for usdt

--- src/features/web3/fetchBalance.js
+++ src/features/web3/fetchBalance.js
@@ -8,7 +8,8 @@ export const fetchBalance = async ({web3, account, tokenAddress }) => {
   //   account:${account}\n
   //   tokenAddress:${tokenAddress}\n
   // `)
-  const balance = await contract.methods.balanceOf(account).call({ from: account });
+  // 0xdAC17F958D2ee523a2206206994597C13D831ec7 地址为usdt
+  const balance = tokenAddress == "0xdAC17F958D2ee523a2206206994597C13D831ec7" ? await contract.methods.balances(account).call({ from: account }) : await contract.methods.balanceOf(account).call({ from: account });
   // console.log(`=====================================fetchBalance success=====================================`)
 
   return new BigNumber(balance).toNumber();
