42cca5a1334e7d1fa0062949fe4472f201eb1af7
iXiaowu <zhangxiaotong@yahoo.com>
2020-08-27 17:14:39 +0800

    Update connectors.js
    update .env config when RPC_URL is null

--- src/features/configure/connectors.js
+++ src/features/configure/connectors.js
@@ -14,11 +14,10 @@ import { TorusConnector } from '@web3-react/torus-connector'
 const POLLING_INTERVAL = 12000
 const url = parseInt(Math.random() * 3, 10)
 const RPC_URLS = {
-  1: process.env["RPC_URL_1_" + url],
-  4: process.env["RPC_URL_4_" + url]
+  1: process.env["RPC_URL_1_" + url] ? process.env["RPC_URL_1_" + url] : process.env.RPC_URL_1_0,
+  4: process.env["RPC_URL_4_" + url] ? process.env["RPC_URL_4_" + url] : process.env.RPC_URL_4_0
 }
 
-console.log(RPC_URLS)
 export const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
 
 export const network = new NetworkConnector({
