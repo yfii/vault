4c7fac90d2860656be1f27bf22fab749dc6e0221
iXiaowu <zhangxiaotong@yahoo.com>
2020-08-31 22:39:23 +0800

    update web3

--- src/features/web3/fetchBalance.js
+++ src/features/web3/fetchBalance.js
@@ -1,15 +1,15 @@
 import { erc20ABI } from "../configure";
 import BigNumber from "bignumber.js";
 
-export const fetchBalance = async ({web3, account, tokenAddress }) => {
+export const fetchBalance = async ({web3, address, tokenAddress }) => {
   // console.log(`=====================================fetchBalance begin=====================================`)
   const contract = new web3.eth.Contract(erc20ABI, tokenAddress)
   // console.log(`
-  //   account:${account}\n
+  //   address:${address}\n
   //   tokenAddress:${tokenAddress}\n
   // `)
   // 0xdAC17F958D2ee523a2206206994597C13D831ec7 地址为usdt
-  const balance = await contract.methods.balanceOf(account).call({ from: account });
+  const balance = await contract.methods.balanceOf(address).call({ from: address });
   // console.log(balance)
   // console.log(`=====================================fetchBalance success=====================================`)
 
--- src/features/web3/fetchClaimAbleTokens.js
+++ src/features/web3/fetchClaimAbleTokens.js
@@ -1,14 +1,14 @@
 import { crvDepositContractABI, strategyContractABI } from "../configure";
 import BigNumber from "bignumber.js";
 
-export const fetchClaimAbleTokens = async ({web3, account, contractAddress, isCrv,crvGauge}) => {
+export const fetchClaimAbleTokens = async ({web3, address, contractAddress, isCrv,crvGauge}) => {
   // console.log(`=====================================fetchClaimAbleTokens begin=====================================`)
   // if (isCrv){
   const contract = isCrv ? new web3.eth.Contract(crvDepositContractABI, crvGauge) : new web3.eth.Contract(strategyContractABI, contractAddress);
-  const data = isCrv ? await contract.methods.claimable_tokens(contractAddress).call({ from: account }) : await contract.methods.balanceOfPendingReward().call({ from: account });
+  const data = isCrv ? await contract.methods.claimable_tokens(contractAddress).call({ from: address }) : await contract.methods.balanceOfPendingReward().call({ from: address });
   
   // console.log(`
-  //   account:${account}\n
+  //   address:${address}\n
   //   contractAddress:${contractAddress}\n
   // `)
   // console.log("data: "+data)
@@ -21,19 +21,21 @@ export const fetchClaimAbleTokens = async ({web3, account, contractAddress, isCr
   return claimAbleTokens;
 }
 
-export const fetchClaimPendingBalance = async ({contract, account, amount}) => {
+export const fetchClaimPendingBalance = async ({contract, address, amount}) => {
   // console.log(`=====================================fetchClaimPendingBalance begin=====================================`)
   // const contract = new web3.eth.Contract(earnContractABI, contractAddress)
   // console.log(`
-  //   account:${account}\n
+  //   address:${address}\n
   //   amount:${amount}\n
   //   contractAddress:${contractAddress}\n
   // `)
+  console.log("claimPendingBalance: begin")
   const data = await contract.methods.cal_out_pending(new BigNumber(amount).dividedToIntegerBy(
     new BigNumber(1)
-  ).toString(), account).call({ from: account });
-  // console.log("data: "+data)
+  ).toString(), address).call({ from: address });
+  console.log("data: "+data)
   const claimPendingBalance = new BigNumber(data).toNumber();
+  console.log("claimPendingBalance: " + claimPendingBalance)
   // console.log(`
   //   claimPendingBalance:${claimPendingBalance}\n
   // `)
