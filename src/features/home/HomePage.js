575437562f0497710de3f2d01d6284190405bfc0
iXiaowu <zhangxiaotong@yahoo.com>
2020-08-31 21:57:54 +0800

    update

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -1,31 +1,13 @@
 import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
 // core components
-import Header from "components/Header/Header.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import FooterLinks from 'components/Footer/FooterLinks.js'
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
 // sections for this page
 import SectionPools from "features/vault/sections/SectionPools.js";
 // style for this page
-import welcomePageStyle from "./jss/welcomePageStyle.js";
 // resource file
 // hooks
-import { useOpenModal } from 'features/common/redux/hooks';
-import { connect } from 'react-redux';
-import { connectWallet } from './redux/actions.js';
 
-const useStyles = makeStyles(welcomePageStyle);
-
-export default function WelcomePage() {
-  const { t } = useTranslation();
-  const { openModal } = useOpenModal();
-  const classes = useStyles();
-
+export default function HomePage() {
   useEffect(() => {
     window.scrollTo(0, 0);
     document.body.scrollTop = 0;
--- src/features/vault/VaultPage.js
+++ src/features/vault/VaultPage.js
@@ -1,37 +1,18 @@
-import React, { useEffect } from 'react';
-import { withRouter } from "react-router";
+import React from 'react';
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 // import Close from "@material-ui/icons/Close";
 // core components
-import Header from "components/Header/Header.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import FooterLinks from 'components/Footer/FooterLinks.js'
 // sections for this page
 import SectionTitle from './sections/SectionTitle';
 import SectionWallet from './sections/SectionWallet';
 import SectionPools from './sections/SectionPools';
 // hooks
-import { useAccount } from 'features/common/redux/hooks';
 
-
-import vaultPageStyle from "./jss/vaultPageStyle";
-
-const useStyles = makeStyles(vaultPageStyle);
-
-function VaultPage(props) {
-  const classes = useStyles();
-  const { account } = useAccount();
-
+export default function VaultPage() {
   window.scrollTo(0, 0);
   document.body.scrollTop = 0;
-
-  useEffect(() => {
-    if (!account) { 
-      return props.history.push('/')
-    }
-  }, [account, props.history]);
   return (
     <>
       <SectionTitle />
@@ -40,5 +21,3 @@ function VaultPage(props) {
     </>
   );
 }
-
-export default withRouter(VaultPage);
